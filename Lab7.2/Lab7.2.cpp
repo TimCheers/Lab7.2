f89c770cfff9ba775488db5219157d44ebe1323d
TimCheers <73362594+TimCheers@users.noreply.github.com>
2020-12-17 16:44:59 +0500

    as

--- Lab7.2/Lab7.2.cpp
+++ Lab7.2/Lab7.2.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <math.h>
 using namespace std;
 
 //Решить указанную в варианте задачу, используя функции с
@@ -9,8 +9,6 @@ using namespace std;
 //Написать функцию(или макроопределение), которая
 //находит площадь треугольника по его сторонам.
 
-
-
 //Написать
 //функцию square c переменным числом параметров, которая
 //находит площадь n - угольника по заданным сторонам и углам.
@@ -18,37 +16,36 @@ using namespace std;
 //функции square не менее трех раз с количеством
 //параметров 3, 5, 8
 
-
-
-
-
-//int nok(int num,int nod, int a, ...)
-//{
-//	int* p = &a;
-//	int Pr = 1;
-//	while (num--)
-//	{
-//		Pr = Pr * *p;
-//		++p;
-//	}
-//	cout << "НОД равен: " << nod<<endl;
-
-
-//int arr[8] = { 0,0,0,0,0,0,0,0 };
-
-
-
-//	cout << "НОК равен: "<<Pr/nod<<endl<<endl;         
-//	return 0;                                        
-//}
-
-int treu(int num, int AB, ...)
+float triangle(float a, float b, float c)
 {
+	float p = (a + b + c) / 2;
+	float S = sqrt(p * (p - a) * (p - b) * (p - c));
+	return S;
+
+}
+
+int square(int num, int AB, ...)
+{
+	int SH=0;
+	if (num < 8 && num < 5)
+	{
+		SH = (num / 2)+2;
+	}
+	if (num < 8 && num == 5)
+	{
+		SH = (num / 2) + 1;
+	}
+	if (num >=8 )
+	{
+		SH = num / 2;
+	}
+	int* arr = new int[SH];
 	int* pr = &AB;
-	int p = 0;
-	int x,k=0;
-	int S, Sall=0;
-	while (num--)
+	float p = 0;
+	int k=0;
+	float x, S, SAll=0;
+	int  SL=num/2, i = 0;
+	while (SL--)
 	{
 		int a, b,co;
 		a = *pr;
@@ -58,23 +55,90 @@ int treu(int num, int AB, ...)
 		b = *pr;
 
 		x = sqrt(pow(a, 2) + pow(b, 2) - 2 * a * b * cos(co));
+		arr[i] = x;
 		p = (a + b + x) / 2;
 		S = sqrt(p * (p - a) * (p - b) * (p - x));
-		Sall += S;
-		++pr;
-	}
-	while (true)
-	{
+		SAll += S;
+		if (num==3)
+		{
+			return SAll;
 
+		}
+		++pr;
+		i++;
+ 	}
+	if (num==5)
+	{
+		arr[SH - 1] = *pr;
 	}
+	p = 0;
+	for (int j = 0; j < SH; j++)
+	{
+		p += arr[j];
+	}
+	p = p / 2;
+	for (int j = 0; j < SH; j++)
+	{
+		int t = arr[j];
+		arr[j] = p - t;
+	}
+	for (int j = 0; j < SH; j++)
+	{
+		p *= arr[j];
+	}
+	S = SAll + sqrt(p);
+	return S;
 
 }
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	
+	int AB=-1, BC=10, CD=-1, DE=-1, EF=-1, FG=-1, GH=-1, HA=-1,CA=-1,EA=-1;
+	int B, D, F, H;
 
+	cout << "Введите параметры для вычисления площади треугольника с помощью функции triangle:" << endl;
+	while ((AB > BC + CA || BC > AB + CA || CA > AB + BC) || (AB <= 0) || (BC <= 0) || (CA <= 0))
+	{
+
+		cout << "AB: ";
+		cin >> AB;
+
+		cout << "BC: ";
+		cin >> BC;
+
+		cout << "CA: ";
+		cin >> CA;
+
+		if ((AB > BC + CA || BC > AB + CA || CA > AB + BC) || (AB <= 0) || (BC <= 0) || (CA <= 0))
+		{
+			cout << "Ошибка! Одна сторона не может быть отрицательной или больше двух других" << endl;
+		}
+	}
+	float x = triangle(AB, BC, CA);
+	cout << "Площадь треугольника ровна  " << x<<endl<<endl;
+	AB = -1; BC = -10;
+	cout << "Введите параметры для вычисления площади треугольника с помощью функции square:" << endl;
+	while ( (AB <= 0) || (BC <= 0) )
+	{
+
+		cout << "AB: ";
+		cin >> AB;
+
+		cout << "BC: ";
+		cin >> BC;
+
+		cout << "Угол между AB И BC: ";
+		cin >> B;
+
+
+		if ((AB > BC + CA || BC > AB + CA || CA > AB + BC) || (AB <= 0) || (BC <= 0) || (CA <= 0))
+		{
+			cout << "Ошибка! Одна сторона не может быть отрицательной или больше двух других" << endl;
+		}
+	}
+	x = square(3, AB, B, BC);
+	cout<< "Площадь треугольника ровна  " << x<<endl;
 }
 
 
